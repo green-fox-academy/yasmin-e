ad2117f6525b2c9e54a954f2683cada98c1740a4
Yasmine <abdelkefiy2@gmail.com>
2020-07-20 14:15:22 +0200

    removing duplicates

--- week-01/day-04/exercises/increment-element.ts
+++
@@ -1,7 +0,0 @@
-// -  Create a variable named `numbers` with the following content: `[1, 2, 3, 4, 5]`
-// -  Increment the third element simply by accessing it
-// -  Log the third element to the console
-'use strict';
-let numbers: number[] = [1, 2, 3, 4, 5];
-numbers[2] = numbers[2] + 1;
-console.log(numbers[2]);
--- week-01/day-04/exercises/swap-elements.ts
+++
@@ -1,7 +0,0 @@
-// -  Create a variable named `names` with the following content:
-//   `["Arthur", "Boe", "Chloe"]`
-// -  Swap the first and the third element of `names`
-'use strict';
-let names: string[] = ["Arthur", "Boe", "Chloe"];
-//[names[0], names[2]] =  [names[2], names[0]];
-console.log(names.reverse());
