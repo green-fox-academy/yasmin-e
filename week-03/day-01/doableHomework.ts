225bb3768ef79114bca6828104dc0483ca1bc717
Yasmine <abdelkefiy2@gmail.com>
2020-07-26 15:55:15 +0200

    doable homework

---
+++ week-03/day-01/doableHomework.ts
@@ -0,0 +1,36 @@
+'use strict';
+let tree1 = {};
+tree1 = {
+    'type': 'sequoia',
+    'leaf color': 'green',
+    'age': 3,
+    'sex': 'male',
+}
+let tree2 = {};
+tree2 = {
+    'type': 'pine',
+    'leaf color': 'green',
+    'age': 60,
+    'sex': 'female',
+}
+let tree3 = {};
+tree3 = {
+    'type': 'oak',
+    'leaf color': 'green',
+    'age': 150,
+    'sex': 'male',
+}
+let tree4 = {};
+tree4 = {
+    'type': 'baobab',
+    'leaf color': 'green',
+    'age': 15,
+    'sex': 'female',
+}
+let tree5 = {};
+tree5 = {
+    'type': 'maple',
+    'leaf color': 'red',
+    'age': 55,
+    'sex': 'male',
+}
