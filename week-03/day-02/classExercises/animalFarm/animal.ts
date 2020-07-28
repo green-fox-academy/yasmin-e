866070e4cdba0b79843d14b24293939364558fe1
Yasmine <abdelkefiy2@gmail.com>
2020-07-28 13:56:44 +0200

    adding animalFarm folder containing animal.ts and farm.ts

---
+++ week-03/day-02/classExercises/animalFarm/animal.ts
@@ -0,0 +1,31 @@
+'use strict';
+/* Create an Animal class
+    Every animal has a hunger value, which is a whole number
+    Every animal has a thirst value, which is a whole number
+    when creating a new animal object these values are created with the default 50 value
+    Every animal can eat() which decreases their hunger by one
+    Every animal can drink() which decreases their thirst by one
+    Every animal can play() which increases both by one */
+
+export class Animal {
+    hunger: number;
+    thirst: number;
+
+    constructor (hunger: number = 50, thirst: number = 50) {
+        this.hunger = hunger;
+        this.thirst = thirst;
+    }
+
+    eat() {
+        this.hunger -= 1;
+    }
+
+    drink() {
+        this.thirst -= 1;
+    }
+
+    play() {
+        this.thirst += 1;
+        this.hunger += 1;
+    }
+}
