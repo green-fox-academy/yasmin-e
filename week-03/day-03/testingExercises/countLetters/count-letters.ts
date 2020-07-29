c80041139dedd918531a89a24086ea3f28723e2f
Yasmine <abdelkefiy2@gmail.com>
2020-07-29 17:09:37 +0200

    final version of count-letters.ts

--- week-03/day-03/testingExercises/countLetters/count-letters.ts
+++ week-03/day-03/testingExercises/countLetters/count-letters.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 /*  Write a function, that takes a string as an argument
 and returns a dictionary with all letters in the string as keys,
 and numbers as values that shows how many occurrences there are.
