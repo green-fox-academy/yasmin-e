6c476290e2619e92b410f43acd1d0e6f3fec4633
Yasmine <abdelkefiy2@gmail.com>
2020-07-30 16:15:00 +0200

    airbnb style guide applied to matrix.ts

--- week-01/day-04/arraysExercises/matrix.ts
+++ week-01/day-04/arraysExercises/matrix.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // -  Create (dynamically*) a two dimensional list
 //    with the following matrix**. Use a loop!
 //
@@ -10,31 +11,21 @@
 //
 // * size should depend on a variable
 // ** Relax, a matrix is just like an array
-'use strict';
-let size: number = 5;
+
+const size = 5;
 let Matrix: number[][] = [];
-let string1: any = " 1";
-let string0: any = " 0";
+const string1: any = ' 1';
+const string0: any = ' 0';
 
-
-for (let i: number =0; i < size; i++) {
-    Matrix[i]= [];
-    for (let j: number = 0; j < size; j++)
- 
-    if (i == j) {
-        Matrix[i][j] = string1;
+for (let i = 0; i < size; i += 1) {
+  Matrix[i] = [];
+  for (let j = 0; j < size; j += 1) {
+    if (i === j) {
+      Matrix[i][j] = string1;
+    } else {
+      Matrix[i][j] = string0;
     }
-    else {
-        Matrix[i][j] = string0;
-    }
+  }
 }
 Matrix = Matrix.reverse();
-console.log(Matrix);
-
-
-for (let i: number = 0; i < Matrix.length; i++) {
-    for (let j: number = 0; j < Matrix.length[i]; j++) {
-    console.log(Matrix[i][j]);
- }
- console.log(`done:${i}`);
-};
+console.log(Matrix.join('\n'));
