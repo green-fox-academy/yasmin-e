55f288dafff5222be260cc1c60a08fc08dcf3a4a
Yasmine <abdelkefiy2@gmail.com>
2020-07-30 16:24:34 +0200

    airbnb styleguide applied to url-fixer.ts

--- week-02/day-02/stringExercises/url-fixer.ts
+++ week-02/day-02/stringExercises/url-fixer.ts
@@ -1,8 +1,7 @@
-'use strict';
 // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
 // Also, the URL is missing a crucial component, find out what it is and insert it too!
 
-let url: string = 'https//www.reddit.com/r/nevertellmethebots';
+const url = 'https//www.reddit.com/r/nevertellmethebots';
 
 console.log(url);
 console.log(url.replace('bots', 'odds').replace('https', 'https:'));
