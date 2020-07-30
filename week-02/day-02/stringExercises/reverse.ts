fc538ba81b7a7b54971d5b45e5d8e8797f4482b0
Yasmine <abdelkefiy2@gmail.com>
2020-07-30 16:20:40 +0200

    airbnb styleguide applied to reverse.ts

--- week-02/day-02/stringExercises/reverse.ts
+++ week-02/day-02/stringExercises/reverse.ts
@@ -1,12 +1,9 @@
-'use strict';
 // Create a function that can reverse a string, which is passed as the parameter
 // Use it on this reversed string to check it!
+const toBeReversed = '.eslaf eb t\'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI';
 
-let toBeReversed: string = `.eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI`;
-export = reverse;
-
-function reverse (value:string) {
-    return value.split("").reverse().join("");
-};
+function reverse(value:string) {
+  return value.split('').reverse().join('');
+}
 
 console.log(reverse(toBeReversed));
