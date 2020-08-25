87e3f0dce7e5f6ef5678c166c473750fe209fb19
Yasmine <yasszmine@Radis-MacBook-Pro.local>
2020-08-25 09:47:27 +0200

    exercise 1 done

---
+++ week-06/day-02/domExercises/exercise-1/test.js
@@ -0,0 +1,12 @@
+let king = document.getElementById('b325');
+console.log(king);
+
+let asteroid = document.querySelectorAll('.asteroid');
+
+let businessLamp = [asteroid[2], asteroid[3]];
+console.log(businessLamp[0]);
+console.log(businessLamp[1]);
+
+let conceitedKing = [asteroid[0], asteroid[1]];
+console.log(conceitedKing[0]);
+console.log(conceitedKing[1]);
