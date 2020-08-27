78a23915b0b33cee7da05da0df22b7ca522be720
Yasmine <yasszmine@Radis-MacBook-Pro.local>
2020-08-27 18:26:05 +0200

    fixing up format

--- week-06/day-02/domExercises/list-manager/main.js
+++ week-06/day-02/domExercises/list-manager/main.js
@@ -1,32 +1,32 @@
-let pageBody = document.querySelector('body');
-let container = document.createElement('div');
+/* eslint-disable no-undef */
+const pageBody = document.querySelector('body');
+const container = document.createElement('div');
 container.setAttribute('class', 'container');
 pageBody.appendChild(container);
 
-let inputBox = document.createElement('div');
+const inputBox = document.createElement('div');
 inputBox.setAttribute('class', 'inputBox');
 
-let inputArr = ['bread', 'milk', 'orange', 'tomato'];
+const inputArr = ['bread', 'milk', 'orange', 'tomato'];
 inputArr.forEach((input) => {
- let newInput = document.createElement('p');
- newInput.textContent = input;
- newInput.setAttribute('class', 'input');
- inputBox.appendChild(newInput);
+  const newInput = document.createElement('p');
+  newInput.textContent = input;
+  newInput.setAttribute('class', 'input');
+  inputBox.appendChild(newInput);
 });
 
-let buttonsBox = document.createElement('div');
+const buttonsBox = document.createElement('div');
 buttonsBox.setAttribute('class', 'buttonsBox');
 
-let buttonsArr = ['Up', '>', 'X', 'Down'];
+const buttonsArr = ['Up', '>', 'X', 'Down'];
 buttonsArr.forEach((button) => {
- let newButton = document.createElement('button');
- newButton.textContent = button;
- newButton.setAttribute('class', 'button');
- buttonsBox.appendChild(newButton);
+  const newButton = document.createElement('button');
+  newButton.textContent = button;
+  newButton.setAttribute('class', 'button');
+  buttonsBox.appendChild(newButton);
 });
 
-
-let outputBox = document.createElement('div');
+const outputBox = document.createElement('div');
 outputBox.setAttribute('class', 'inputBox');
 
 container.appendChild(inputBox);
@@ -36,55 +36,55 @@ container.appendChild(outputBox);
 let inputs = document.querySelectorAll('.input');
 inputs[0].classList.add('selected');
 
-let buttons = document.querySelectorAll('button');
-let up = buttons[0];
-let down = buttons[3];
-let move = buttons[1];
-let remove = buttons[2];
+const buttons = document.querySelectorAll('button');
+const up = buttons[0];
+const down = buttons[3];
+const move = buttons[1];
+const remove = buttons[2];
 
 up.onclick = () => {
- for (let i = 1; i < inputs.length; i += 1) {
-  if (inputs[i].classList.contains('selected')) {
-   inputs[i].classList.remove('selected');
-   inputs[i - 1].classList.add('selected');
-   break;
-  } 
- }
+  for (let i = 1; i < inputs.length; i += 1) {
+    if (inputs[i].classList.contains('selected')) {
+      inputs[i].classList.remove('selected');
+      inputs[i - 1].classList.add('selected');
+      break;
+    }
+  }
 };
 
 down.onclick = () => {
- for (let i = 0; i < inputs.length - 1; i += 1) {
-  if (inputs[i].classList.contains('selected')) {
-   inputs[i].classList.remove('selected');
-   inputs[i + 1].classList.add('selected');
-   break;
+  for (let i = 0; i < inputs.length - 1; i += 1) {
+    if (inputs[i].classList.contains('selected')) {
+      inputs[i].classList.remove('selected');
+      inputs[i + 1].classList.add('selected');
+      break;
+    }
   }
- }
 };
 
 remove.onclick = () => {
- for (let i = 0; i < inputs.length; i += 1) {
-  if (inputs[i].classList.contains('selected')) {
-   inputs[i].classList.remove('selected');
-   inputs[i].remove();
-   inputs = document.querySelectorAll('.input');
-   inputs[0].classList.add('selected');
-   break;
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (inputs[i].classList.contains('selected')) {
+      inputs[i].classList.remove('selected');
+      inputs[i].remove();
+      inputs = document.querySelectorAll('.input');
+      inputs[0].classList.add('selected');
+      break;
+    }
   }
- }
-}
+};
 
 move.onclick = () => {
- for (let i = 0; i < inputs.length; i += 1) {
-  if (inputs[i].classList.contains('selected')) {
-   newOutput = document.createElement('p');
-   newOutput.textContent = inputs[i].textContent;
-   newOutput.setAttribute('class', 'input');
-   outputBox.appendChild(newOutput);
-   inputs[i].classList.remove('selected');
-   inputs = document.querySelectorAll('.input');
-   inputs[0].classList.add('selected');
-   break;
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (inputs[i].classList.contains('selected')) {
+      newOutput = document.createElement('p');
+      newOutput.textContent = inputs[i].textContent;
+      newOutput.setAttribute('class', 'input');
+      outputBox.appendChild(newOutput);
+      inputs[i].classList.remove('selected');
+      inputs = document.querySelectorAll('.input');
+      inputs[0].classList.add('selected');
+      break;
+    }
   }
- }
-}
+};
