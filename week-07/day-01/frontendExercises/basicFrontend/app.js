de3f52cdb9f9f45cd1df0ececb9fda562e91e3c6
Yasmine <yasszmine@radis-mbp.t.hu>
2020-08-31 16:15:15 +0200

    server up and running static files used

--- week-07/day-01/frontendExercises/basicFrontend/app.js
+++ week-07/day-01/frontendExercises/basicFrontend/app.js
@@ -1,14 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const express = require('express');
+const path = require('path');
 
 const app = express();
-
 const port = 3000;
 
+app.use(express.static('assets'));
+
 app.get('/', (req, res) => {
-  res.send('OK!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`connecting through port ${port}`);
+  console.log(`The server is up and running on ${port}`);
 });
