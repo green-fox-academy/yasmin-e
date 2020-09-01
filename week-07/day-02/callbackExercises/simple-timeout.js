a8e1a78456143f5f77e58d199df832b74d62bc5c
Yasmine <yasszmine@192.168.1.8>
2020-09-01 13:59:01 +0200

    minor refractoring removing console log on settimeout

--- week-07/day-02/callbackExercises/simple-timeout.js
+++ week-07/day-02/callbackExercises/simple-timeout.js
@@ -1,5 +1,3 @@
 // Write a program that prints apple after 3 seconds
 const printApple = () => { console.log('Apple'); };
-const printApple3sec = setTimeout(printApple, 3000);
-
-console.log(printApple3sec);
+setTimeout(printApple, 3000);
