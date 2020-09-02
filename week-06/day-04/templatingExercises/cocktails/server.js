1058c36932462788043ffd04ed09cef4f5b830d2
Yasmine <yasszmine@192.168.1.8>
2020-09-02 14:28:54 +0200

    added buttons

--- week-06/day-04/templatingExercises/cocktails/server.js
+++ week-06/day-04/templatingExercises/cocktails/server.js
@@ -5,9 +5,11 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+
 app.listen(port, () => {
   console.log(`connected through port ${port}`);
 });
+
 app.use('/static', express.static('static'));
 
 app.get('/', (req, res) => {
---
+++ week-06/day-04/templatingExercises/cocktails/static/index.js
@@ -0,0 +1,38 @@
+/* eslint-disable no-loop-func */
+const buttons = document.querySelectorAll('button');
+
+const all = buttons[0];
+const gin = buttons[1];
+const vodka = buttons[2];
+const rum = buttons[3];
+const tequila = buttons[4];
+
+all.onclick = () => {
+  const urlBase = 'http://localhost:3000/';
+  const url = `${urlBase}`;
+  document.location.href = url;
+};
+
+gin.onclick = () => {
+  const urlBase = 'http://localhost:3000/?alcohol=';
+  const url = `${urlBase}gin`;
+  document.location.href = url;
+};
+
+vodka.onclick = () => {
+  const urlBase = 'http://localhost:3000/?alcohol=';
+  const url = `${urlBase}vodka`;
+  document.location.href = url;
+};
+
+rum.onclick = () => {
+  const urlBase = 'http://localhost:3000/?alcohol=';
+  const url = `${urlBase}rum`;
+  document.location.href = url;
+};
+
+tequila.onclick = () => {
+  const urlBase = 'http://localhost:3000/?alcohol=';
+  const url = `${urlBase}tequila`;
+  document.location.href = url;
+};
