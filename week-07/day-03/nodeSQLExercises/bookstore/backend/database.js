1938cf27ecace3292aea356a34aca3685a696b42
Yasmine <yasszmine@192.168.1.6>
2020-09-09 14:51:51 +0200

    successfully connected to DB

---
+++ week-07/day-03/nodeSQLExercises/bookstore/backend/database.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+const databaseConn = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+databaseConn.connect((err) => {
+  if (err) {
+    console.error('Unable to connect to DB', err.sqlMessage);
+    return;
+  }
+  console.log('Successfully connected to DB');
+});
+
+module.exports = databaseConn;
