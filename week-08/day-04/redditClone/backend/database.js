3dbd52d8b5251bfa67f931d4084ed59bcbdbb9e3
Yasmine <yasszmine@192.168.1.6>
2020-09-10 13:47:49 +0200

    feat() initial database connexion established

---
+++ week-08/day-04/redditClone/backend/database.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+conn.connect((err) => {
+  if (err) {
+    console.error('Unable to connect to DB', err.sqlMessage);
+    return;
+  } console.log('Successfully connected to DB');
+});
+
+module.exports = conn;
