92cd6bba0fc899a87c72d27c1b3ada6064b961c2
Yasmine <yasszmine@192.168.1.6>
2020-09-10 15:38:55 +0200

    feat() post /posts endpoint working

--- week-08/day-04/redditClone/backend/endpoints.js
+++ week-08/day-04/redditClone/backend/endpoints.js
@@ -9,13 +9,45 @@ router.get('/', (req, res) => {
 
 router.get('/posts', (req, res) => {
   res.header('Content-Type', 'application/json');
-  const sqlQuery = 'SELECT * FROM posts';
-  conn.query(sqlQuery, (err, result) => {
+
+  const getPosts = 'SELECT * FROM posts';
+
+  conn.query(getPosts, (err, posts) => {
     if (err) {
       console.error(err.sqlMessage);
       res.sendStatus(500); // internal server error
       return null;
-    } return res.status(200).json({ posts: result });
+    } return res.status(200).json({ posts });
+  });
+});
+
+router.post('/posts', (req, res) => {
+  res.header('Content-type', 'application/json');
+  const newPost = req.body;
+
+  const insertNewPost = `INSERT INTO posts (title, url) 
+  VALUES (${newPost.title}, ${newPost.url})`;
+  const getNewPost = `SELECT * FROM posts 
+  WHERE posts.title=${newPost.title} AND posts.url=${newPost.url}`;
+
+  conn.query(insertNewPost, (err1, result) => {
+    if (err1) {
+      console.error(err1.sqlMessage);
+      res.sendStatus(500);
+      return null;
+    }
+
+    conn.query(getNewPost, (err2) => {
+      if (err2) {
+        console.error(err2.sqlMessage);
+        res.sendStatus(500);
+        return null;
+      }
+      const newPostfromDB = result;
+      return newPostfromDB;
+    });
+
+    return res.status(200).json(result);
   });
 });
 
