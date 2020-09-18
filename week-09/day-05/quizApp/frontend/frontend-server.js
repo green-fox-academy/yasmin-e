71d43a7900deeef4a71cf8b4ade5ee4476518aaf
Yasmine <yasszmine@Radis-MacBook-Pro.local>
2020-09-18 13:18:37 +0200

    frontend endpoints done

--- week-09/day-05/quizApp/frontend/frontend-server.js
+++ week-09/day-05/quizApp/frontend/frontend-server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = 8080;
@@ -8,4 +9,12 @@ app.use(express.static('static'));
 
 app.listen(port, () => {
   console.log(`Frontend server running on port ${port}`);
+});
+
+app.get('/game', (req, res) => {
+  res.sendFile(path.join('index.html'));
+});
+
+app.get('/questions', (req, res) => {
+  res.sendFile(path.join('index.html'));
 });
