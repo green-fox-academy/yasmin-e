e71cb5bf2dd5310ea7ab2b265450934526a2910d
Yasmine <yasszmine@radis-mbp.t.hu>
2020-09-22 17:21:32 +0200

    refractoring the add tracks method to comply with new table fields

--- week-10/foxplayer/backend/routes/post-tracks.js
+++ week-10/foxplayer/backend/routes/post-tracks.js
@@ -5,9 +5,12 @@ const postTracks = express.Router();
 
 postTracks.post('/:playlist_id', (req, res) => {
   const { path } = req.body;
+  const { title } = req.body;
+  const { artist } = req.body;
+  const { duration } = req.body;
   const id = req.params.playlist_id;
-  const addTrack = 'INSERT INTO tracks(path, playlist_id) VALUES(?,?);';
-  query(addTrack, [path, id])
+  const addTrack = 'INSERT INTO tracks(title, artist, duration, path, playlist_id) VALUES(?,?,?,?,?);';
+  query(addTrack, [title, artist, duration, path, id])
     .then((tracks) => res.status(200).json(tracks))
     .catch((error) => {
       res.sendStatus(500);
